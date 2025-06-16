13244f1039a531d973d2285e6db810021e1e0bf7
sawantbandita <143108155+sawantbandita@users.noreply.github.com>
2025-06-16 17:36:30 +0530

    Add files via upload

---
+++ Monday_Coffee_Data_Analysis.sql
@@ -0,0 +1,288 @@
+--Monday Coffee -- Data Analysis
+
+select *
+from city;
+
+select *
+from products;
+
+
+select *
+from customers;
+
+select *
+from sales;
+
+--Reports and Data Analysis
+
+-- Q1. Coffee Consumers Count
+--How many people in each city are estimated to consume coffee, given that 25% of the population does?
+
+select city_name,
+		round((population* 0.25)/1000000,2) as coffee_consumers_in_millions,
+		city_rank
+from city
+order by 2 desc;
+
+--Q2. Total Revenue from Coffee Sales
+--What is the total revenue generated from coffee sales across all cities in the last quarter of 2023?
+	
+select ci.city_name,sum(total) as total_revenue
+from sales as s
+join customers as c
+on s.customer_id=c.customer_id
+join city as ci
+on ci.city_id=c.city_id
+where extract(year from s.sale_date) = 2023 and extract(quarter from s.sale_date)=4
+group by 1
+order by 2 desc;
+
+--Q3.Sales Count for Each Product
+--How many units of each coffee product have been sold?
+
+select p.product_name,count(s.sale_id) as total_orders
+from products as p
+left join sales as s
+on s.product_id = p.product_id
+group by 1
+order by 2 desc;
+
+--Q4.Average Sales Amount per City
+--What is the average sales amount per customer in each city?
+
+select ci.city_name,
+		sum(s.total) as total_revenue,
+		count(distinct s.customer_id) as total_cx,
+		round((sum(s.total) / count(distinct s.customer_id))::numeric, 2) as per_customer
+
+from sales as s
+join customers as c
+on s.customer_id=c.customer_id
+join city as ci
+on ci.city_id=c.city_id
+group by 1
+order by 2 desc;
+
+
+--Q5.City Population and Coffee Consumers
+--Provide a list of cities along with their populations and estimated coffee consumers.
+
+with city_table as
+(
+select city_name, round((population * 0.25)/1000000) as coffee_consumers
+from city
+),
+
+customers_table as
+(
+select ci.city_name , count(distinct c.customer_id) as unique_cx
+from sales as s 
+join customers as c
+on c.customer_id = s.customer_id
+join city as ci
+on ci.city_id = c.city_id
+group by 1
+)
+select 
+		customers_table.city_name,city_table.coffee_consumers as coffee_cons_In_million,customers_table.unique_cx
+from city_table
+join customers_table
+on city_table.city_name = customers_table.city_name;
+
+
+--Q6. Top selling Products by City
+--WHat are the top 3 selling products in each city based on sales volumes?
+
+
+select *
+from
+(
+SELECT 
+		ci.city_name,
+		p.product_name,
+		count(s.sale_id)as total_orders,
+		dense_rank() over(partition by ci.city_name order by count (s.sale_id) desc) as rank
+FROM sales AS s
+JOIN products AS p ON s.product_id = p.product_id
+JOIN customers AS c ON c.customer_id = s.customer_id
+JOIN city AS ci ON ci.city_id = c.city_id
+group by 1,2)
+where rank<=3;
+--order by 1,3 desc;
+
+
+--Q7.Customer Segmentation by City
+--How many unique customers are there in each city who have purchased coffee products?
+
+select 
+		ci.city_name,
+		count(distinct c.customer_id) as unique_cx
+from city as ci
+left join
+customers as c
+on c.city_id = ci.city_id
+join sales as s
+on s.customer_id = c.customer_id
+where 
+	s.product_id in (1,2,3,4,5,6,7,8,9,10,11,12,13,14)
+	group by 1;
+
+
+--Q8.Average Sale vs Rent
+--Find each city and their average sale per customer and avg rent per customer
+
+with city_table
+as
+(
+select ci.city_name,
+		sum(s.total) as total_revenue,
+		count(distinct s.customer_id) as total_cx,
+		round((sum(s.total) / count(distinct s.customer_id))::numeric, 2) as per_customer
+
+from sales as s
+join customers as c
+on s.customer_id=c.customer_id
+join city as ci
+on ci.city_id=c.city_id
+group by 1
+order by 2 desc
+),
+city_rent 
+as 
+(select
+		city_name,
+		estimated_rent
+from city)
+
+select 
+	cr.city_name,
+	cr.estimated_rent,
+	ct.total_cx,
+	ct.per_customer,
+	round(
+	cr.estimated_rent :: numeric/ct.total_cx :: numeric,2) as avg_rent_per_cx
+from city_rent as cr 
+join city_table as ct
+on cr.city_name = ct.city_name
+order by 5 desc;
+
+
+--Q9. Monthly Sales Growth
+--Sales growth rate: Calculate the percentage growth (or decline) in sales over different time periods (monthly).
+
+with 
+monthly_sales as
+(
+select 
+	ci.city_name,
+	extract( month from sale_date)as month,
+	extract( year from sale_date)as year,
+	sum(s.total) as total_sales
+	
+from sales as s
+join customers as c
+on c.customer_id = s.customer_id
+join city as ci
+on ci.city_id = c.city_id
+group by 1,2,3
+order by 1,3,2 
+),
+
+growth_ratio
+as
+(
+select city_name,
+		month,
+		year,
+		total_sales as cr_month_sale,
+		lag(total_sales,1) over (partition by city_name order by year,month) as last_month_sale
+from monthly_sales
+)
+
+select 
+	city_name,
+	month,
+	year,
+	cr_month_sale,
+	last_month_sale,
+	round
+	((cr_month_sale - last_month_sale)::numeric/last_month_sale::numeric * 100,2) as growth_ratio
+from growth_ratio
+where last_month_sale is not null;
+
+
+--Q10. Market Potential Analysis
+--Identify top 3 city based on highest sales, return city name, total sale, total rent, total customers, estimated coffee consumer
+
+
+with city_table
+as
+(
+select ci.city_name,
+		sum(s.total) as total_revenue,
+		count(distinct s.customer_id) as total_cx,
+		round((sum(s.total) / count(distinct s.customer_id))::numeric, 2) as per_customer
+
+from sales as s
+join customers as c
+on s.customer_id=c.customer_id
+join city as ci
+on ci.city_id=c.city_id
+group by 1
+order by 2 desc
+),
+city_rent 
+as 
+(select
+		city_name,
+		estimated_rent,
+		round((population * 0.25)/1000000,3) as estimated_coffee_cunsumer_in_million
+from city)
+
+select 
+	cr.city_name,
+	total_revenue,
+	cr.estimated_rent as total_rent,
+	ct.total_cx,
+	estimated_coffee_cunsumer_in_million,
+	ct.per_customer,
+	round(
+	cr.estimated_rent :: numeric/ct.total_cx :: numeric,2) as avg_rent_per_cx
+from city_rent as cr 
+join city_table as ct
+on cr.city_name = ct.city_name
+order by 2 desc;
+
+
+
+/*
+--Recomendations
+
+City 1: Pune
+
+Average rent per customer is very low.
+Highest total revenue.
+Average sales per customer is also high.
+City 2: Delhi
+
+Highest estimated coffee consumers at 7.7 million.
+Highest total number of customers, which is 68.
+Average rent per customer is 330 (still under 500).
+City 3: Jaipur
+
+Highest number of customers, which is 69.
+Average rent per customer is very low at 156.
+Average sales per customer is better at 11.6k.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
